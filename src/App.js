4f7293c3491abf0c053f9cd99bfab7daab8caabb
divyendumaurya <rajatmaurya0720@gmail.com>
2023-07-12 18:24:27 +0530

    corrected
    correction textform with html braces

--- src/App.js
+++ src/App.js
@@ -2,13 +2,13 @@
 // import { useState } from 'react';
 // import { Alert } from 'bootstrap';
 import './App.css';
-import About from './components/About';
+// import About from './components/About';
 
 import Navbar from './components/Navbar';
 import TextForm from './components/TextForm';
 import Alert from './components/Alert';
 import React, {useState} from 'react'
-import {BrowserRouter as Router , Route , Link ,Routes } from "react-router-dom";
+// import {BrowserRouter as Router , Route , Link ,Routes } from "react-router-dom";
 
 
 function App() {
@@ -65,27 +65,27 @@ const toggleModeGreen=()=>{
   return (
     <>
     
-<Router>
+{/* <Router> */}
 <Navbar title= "TextUtils" about= "AboutTextUtils" mode={mode} toggleMode={toggleMode} greenMode={greenMode} toggleModeGreen={toggleModeGreen} />
 
 
 <Alert alert={alert}/>
 
   
-    <Routes>
-          <Route path="/about" element={< About />}>
+    {/* <Routes> */}
+          {/* <Route path="/about" element={< About />}> */}
             {/* <About /> */}
           
           
-          </Route>
-          <Route path="/" element= {<TextForm showAlert={showAlert} heading = "Enter Your Text Here" mode={mode} />}>
+          {/* </Route> */}
+          {/* <Route path="/" element= {<TextForm showAlert={showAlert} heading = "Enter Your Text Here" mode={mode} />}> */}
             
-          </Route>
-        </Routes>
+          {/* </Route> */}
+        {/* </Routes> */}
 
-
+       < TextForm showAlert={showAlert} heading = "Enter Your Text Here" mode={mode} />
       
-</Router>
+{/* </Router> */}
 
               
             {/* <About/> */}
